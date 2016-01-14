bee9b1b1e0a148b17bf43844ebc48ab1412df439
Davis Nygren <nygrendr@rose-hulman.edu>
2016-01-14 14:45:38 -0500

    Enabled Command Line arguments

--- src/asm/asmVisitor/SequenceMain.java
+++ src/asm/asmVisitor/SequenceMain.java
@@ -18,7 +18,7 @@ public class SequenceMain {
     public static void main(String[] args) throws IOException {
         OutputStream sdOut = new FileOutputStream("inputOutput/seqOutput.sd");
         SequenceOutputStream seqOut = new SequenceOutputStream(sdOut);
-        //String methodSignature = args[0];
+        String methodSignature = args[0];
         int maxCallDepth = args.length < 2 ? 5 : Integer.parseInt(args[1]);
         if(maxCallDepth == 0)
             return;
@@ -26,7 +26,18 @@ public class SequenceMain {
 
         String className = "asm.asmVisitor.ClassMethodVisitor"; //TODO actual input
         String methodName = "visitMethod";
-        maxCallDepth = 2;
+//        maxCallDepth = 2;
+
+//        String methodSignature = "asm.asmVisitor.ClassMethodVisitor.visitMethod(String arg1, int arg2)";
+        String[] split = methodSignature.split("[.]");
+        methodName = split[split.length-1].split("[(]")[0];
+//        System.out.println(split[split.length-2]);
+//        System.out.println(methodSignature.substring(0, methodSignature.charAt('.')));
+        className=split[0];
+        for(int i=1; i<split.length-1; i++) {
+            className+="."+split[i];
+        }
+        System.out.println(className);
 
         Method topMethod = SequenceRunner.run(className,methodName,maxCallDepth);
 
